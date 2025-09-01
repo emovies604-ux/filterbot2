87a9b0f1124600f3316aeda2dab49963773816ee
emovies604-ux <emovies604@gmail.com>
2025-09-01 23:59:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 27020363  # Replace with your actual API ID
 API_HASH = "900133bfe09ce6ef78885e3599ba64ca"
-BOT_TOKEN = ""
+BOT_TOKEN = "8334188484:AAFIJACJ0YPy9LhX3ONh4FztqX47mz7ZC3c"
 MONGO_URI = "mongodb+srv://Billobb:Billobb@billobb.v67fmki.mongodb.net/?retryWrites=true&w=majority&appName=billobb"
 
 LOG_CHANNEL_ID = -1002967483523  # Channel where bot logs status
@@ -8,3 +8,4 @@ FILE_CHANNELS = [-1002789851054]  # Channels to search files from
 
 
 ADMINS = [1222287481]  # Your Telegram ID
+
